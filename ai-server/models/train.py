213b7039e4fd71e8acee6987a319c61589fd3272
cocese1004 <cocese23334@gmail.com>
2026-05-10 15:45:26 +0900

    KAN-13: train.py 및 dataset 업데이트 (모델 파일 제외)

--- ai-server/models/train.py
+++ ai-server/models/train.py
@@ -63,14 +63,26 @@ class ReviewDataset(Dataset):
 # 훈련 클래스
 class ModelTrain:
     def __init__(self, config: dict) -> None:
-        self._model_name: str = config["model_name"] # 모델 이름
-        self._tokenizer = AutoTokenizer.from_pretrained(self._model_name) # 토크나이저
-        self._model: nn.Module = AutoModelForSequenceClassification.from_pretrained(
-            self._model_name,
-            num_labels=config["num_labels"]
-        ) # 모델
-        if config["load_my_model"]:
-            pass
+        self._model_name: str = config["model_name"]  # 모델 이름
+
+        # 모델 불러오기
+        if config["load_my_model"]: # load_my_model: bool
+            try:
+                self._tokenizer = AutoTokenizer.from_pretrained(self._model_name, local_file_only=True)  # 토크나이저
+                self._model: nn.Module = AutoModelForSequenceClassification.from_pretrained(
+                    self._model_name,
+                    num_labels=config["num_labels"],
+                    local_files_only=True
+                ) # 모델
+            except:
+                raise ValueError("해당 모델이 존재하지 않습니다.")
+        else:
+            self._tokenizer = AutoTokenizer.from_pretrained(self._model_name) # 토크나이저
+            self._model: nn.Module = AutoModelForSequenceClassification.from_pretrained(
+                self._model_name,
+                num_labels=config["num_labels"]
+            ) # 모델
+
 
         # config data
         self._config = config
@@ -78,9 +90,7 @@ class ModelTrain:
         self._epochs = config["epochs"]
         self._device = torch.device(config["device"])
 
-        self._optimizer = None
-
-        self._set_optimizer(config["optimizer"], config["lr"])
+        self._optimizer = self._set_optimizer(self._model, config["optimizer"], config["lr"])
 
     @classmethod
     def load_all_csv_data(cls, directory_path, file_labels):
@@ -106,9 +116,10 @@ class ModelTrain:
 
         return combined_df
 
-    def _set_optimizer(self, optim_name: str, lr=2e-5) -> None:
+    @classmethod
+    def _set_optimizer(cls, model, optim_name: str, lr=2e-5) -> None:
         optimizer = getattr(torch.optim, optim_name) # AdamW 추천
-        self._optimizer = optimizer(self._model.parameters(), lr=lr)
+        return optimizer(model.parameters(), lr=lr)
 
     def _train_dataloader(self, dataset: Dataset) -> DataLoader:
         return DataLoader(dataset, batch_size=self._batch_size, shuffle=True)
@@ -156,7 +167,10 @@ class ModelTrain:
     def train(self, train_dataset: Dataset, valid_dataset: Dataset = None, save_config=False, valid=False) -> None:
         train_loader: DataLoader = self._train_dataloader(train_dataset)
         if valid:
-            valid_loader: DataLoader = self._valid_dataloader(valid_dataset)
+            try:
+                valid_loader: DataLoader = self._valid_dataloader(valid_dataset)
+            except:
+                raise ValueError("검증이 활성화 되어있으나 검증 데이터셋이 할당되지 않았습니다.")
 
 
         for epoch in range(self._epochs):
@@ -169,7 +183,7 @@ class ModelTrain:
 
             print(f"Epoch {epoch+1}/{self._epochs} - Train Loss: {total_loss/len(train_loader):.4f}")
             if valid:
-                self.valid()
+                self.valid(valid_loader)
 
         self.save(save_config=save_config)
 
@@ -181,6 +195,8 @@ class ModelTrain:
 
         for batch in valid_loader:
             loss, logits, labels = self._valid_step(batch)
+
+            total_loss += loss
 
             probs = torch.softmax(logits, dim=-1)
             preds = torch.argmax(probs, dim=-1)
@@ -213,43 +229,60 @@ class ModelTrain:
 
 
 if __name__ == "__main__":
+    # 모델 config
     train_config = {
         "model_name": "beomi/kcbert-base",
         "num_labels": 2,
-        "batch_size": 8,
-        "epochs": 3,
-        "lr": 2e-5,
+        "batch_size": 32,
+        "epochs": 5,
+        "lr": 1e-5,
         "optimizer": "AdamW",
         "load_my_model": False,
         "device": "cpu"
     }
 
-    data_labels = {
-        "coffee_professional_reviews2.csv": 1,
+    train_data_labels = {
         "coffee_reviews.csv": 1,
         "dessert_reviews.csv": 0,
         "drink_reviews.csv": 0,
         "employee_reviews.csv": 0,
-        "interio_reviews.csv": 0,
+        "interior_reviews.csv": 0,
         "restaurant_reviews.csv": 0,
         "tmi_reviews.csv": 0
     }
-    train_data_df = ModelTrain.load_all_csv_data("train_dataset", data_labels)
+
+    valid_data_labels = {
+        "coffee_reviews.csv": 1,
+        "dessert_reviews.csv": 0,
+        "drink_reviews.csv": 0,
+        "employee_reviews.csv": 0,
+        "interior_reviews.csv": 0,
+        "restaurant_reviews.csv": 0,
+        "tmi_reviews.csv": 0
+    }
+
+    train_data_df = ModelTrain.load_all_csv_data("train_dataset", train_data_labels)
+    valid_data_df = ModelTrain.load_all_csv_data("valid_dataset", valid_data_labels)
 
     if not train_data_df.empty:
-        texts = train_data_df["text"].tolist()
-        labels = train_data_df["label"].tolist()
-        print(f"커피 관련 리뷰 개수: {labels.count(1)}\n커피 관련 없는 리뷰 개수: {labels.count(0)}")
+        train_texts = train_data_df["text"].tolist()
+        train_labels = train_data_df["label"].tolist()
+        train_config["coffee_review_train_data"] = train_labels.count(1)
+        train_config["not_coffee_review_train_data"] = train_labels.count(0)
+        print(f"커피 관련 리뷰 개수(훈련): {train_labels.count(1)}\n커피 관련 없는 리뷰 개수(훈련): {train_labels.count(0)}")
 
-        train_texts, valid_texts, train_labels, valid_labels = train_test_split(
-            texts, labels, test_size=0.2, random_state=42
-        )
+        valid_texts = valid_data_df["text"].tolist()
+        valid_labels = valid_data_df["label"].tolist()
+        train_config["coffee_review_valid_data"] = train_labels.count(1)
+        train_config["not_coffee_review_valid_data"] = train_labels.count(0)
+        print(f"커피 관련 리뷰 개수(검증): {train_labels.count(1)}\n커피 관련 없는 리뷰 개수(검증): {train_labels.count(0)}")
+
 
         model_trainer = ModelTrain(train_config)
-        train_dataset = ReviewDataset(train_texts, train_labels, model_trainer._tokenizer)
-        valid_dataset = ReviewDataset(valid_texts, valid_labels, model_trainer._tokenizer)
+        train_dataset = ReviewDataset(train_texts, train_labels, model_trainer._tokenizer, max_length=256)
+        valid_dataset = ReviewDataset(valid_texts, valid_labels, model_trainer._tokenizer, max_length=256)
         print(f"훈련 데이터셋 개수: {len(train_dataset)}\n검증 데이터셋 개수: {len(valid_dataset)}")
 
-        model_trainer.train(train_dataset, save_config=True, valid=True)
+        model_trainer.train(train_dataset, valid_dataset, save_config=True, valid=True)
     else:
         print("train_dataset 폴더에서 csv 파일들을 찾을 수 없습니다.")
